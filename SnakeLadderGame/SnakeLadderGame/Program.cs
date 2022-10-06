5c33766ab883d482401118d769457fbeccd1996a
nrp033 <pawarnitin9433@gmail.com>
2022-10-06 23:03:41 +0530

    Starting Game for single player Starting Position )

---
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SnakeLadderGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t ****Welcome To Snake and Ladder Game****");
+            Console.WriteLine("\n GAME STARTED\n");
+            int playerPosition = 0;
+            Console.WriteLine("Player Position :  " + playerPosition);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
