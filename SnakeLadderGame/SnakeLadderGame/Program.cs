ea8cf1f5c10f33abb6b6d13e3c262db49712a35e
nrp033 <pawarnitin9433@gmail.com>
2022-10-06 23:15:20 +0530

    Using Random Rolling Dice To get Number Between 1 to 6

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -5,10 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\n\t ****Welcome To Snake and Ladder Game****");
-            Console.WriteLine("\n GAME STARTED\n");
-            int playerPosition = 0;
-            Console.WriteLine("Player Position :  " + playerPosition);
 
+            Console.WriteLine("\n GAME STARTED\n");
+
+            int Position = 0;
+            Console.WriteLine("Player Position :  "+Position);
+
+            Console.Write("\nPress Enter To Roll Dice : ");
+            Console.ReadKey();
+
+            Random rd = new Random();   
+            int RollDice=rd.Next(1,7);
+
+            Console.WriteLine("\nDice Roll :  "+RollDice);
 
 
             Console.ReadKey();
