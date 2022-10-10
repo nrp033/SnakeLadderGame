79ade1968e45dc2544912609305178c8ecd99986
nrp033 <pawarnitin9433@gmail.com>
2022-10-10 18:29:53 +0530

    Play Continue Till Player Reaches Exact WinPoint 100

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -15,37 +15,44 @@
             const int WinPoint = 100;
             Console.WriteLine("Player Position :  " + playerPosition);
 
-            while (playerPosition <= WinPoint)
+            while (playerPosition < WinPoint)
             {
                 Random rd = new Random();
                 int rolldice = rd.Next(1, 7);
-                Console.WriteLine(" Dice Roll : " + rolldice);
                 int option = rd.Next(0, 3);
                 switch (option)
                 {
                     case Ladder:
+
                         Console.WriteLine(" It's Ladder ! ");
+                        Console.WriteLine(" Dice Roll : " + rolldice);
                         playerPosition = playerPosition + rolldice;
                         break;
+
                     case Snake:
                         Console.WriteLine(" Snake Bite !");
-                        playerPosition = playerPosition - rolldice;
+                        int rolldice1 = rd.Next(1, 7);
+                        Console.WriteLine(" Dice Roll : " + rolldice1);
+                        playerPosition = playerPosition - rolldice1;
                         break;
 
                     default:
                         Console.WriteLine(" No Play !");
-
                         break;
+
                 }
                 Console.WriteLine("Current Player Position :   " + playerPosition);
                 if (playerPosition < 0)
                 {
                     playerPosition = 0;
                 }
+                else if (playerPosition > WinPoint)
+                {
+                    playerPosition -= rolldice;
+                }
 
             }
-            Console.ReadLine();
-
+            Console.ReadKey();
         }
     }
 }
